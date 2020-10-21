3ff37db1f79d2a58ed65396e3d30b92e5428492a
NoNotCar <13haslat@gmail.com>
2020-10-21 15:50:38 +0100

    Initial PDF generation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
 
 namespace Gearset_PDF
 {
@@ -6,7 +10,13 @@ namespace Gearset_PDF
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PdfDocument pdfDocument = new PdfDocument(new PdfWriter(new FileStream(Directory.GetCurrentDirectory()+"/hello.pdf", FileMode.Create, FileAccess.Write)));
+            Document document = new Document(pdfDocument);
+
+            String line = "Hello! Welcome to iTextPdf";
+            document.Add(new Paragraph(line));
+            document.Close();
+            Console.WriteLine("Awesome PDF just got created.");
         }
     }
 }
